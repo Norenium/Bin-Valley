dd922dd10135aff6c4884904d998a9baefbf9f6b
ElizabethBernard <development.noren@gmail.com>
2022-06-17 15:30:34 +0430

    add style and js

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+document.getElementById('connect-wallet-btn').addEventListener("click",ConnctWallet);
+
+function ConnctWallet(){
+      console.log('connect-wallet-btn clicked.')
+      document.getElementById('connect-wallet-div').style.display = "none";
+}
