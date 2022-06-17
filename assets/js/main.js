6fabfa3e802872b796483923d707fb184db8a19e
ElizabethBernard <development.noren@gmail.com>
2022-06-17 16:25:29 +0430

    information board added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
 document.getElementById('connect-wallet-btn').addEventListener("click",ConnctWallet);
+document.getElementById('health-title').addEventListener("click",CH);
 
 function ConnctWallet(){
       console.log('connect-wallet-btn clicked.')
       document.getElementById('connect-wallet-div').style.display = "none";
+}
+
+
+function CH(){
+      console.log('CH clicked.')
+      document.getElementById("health-bar").ariaValueNow = 50;
+      document.getElementById("health-bar").style.width = '50%';
 }
